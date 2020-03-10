972b0bd8a82d5737e12448c80ee9d4c0d4b19dc5
Peter Buchegger <peter@pbuchegger.at>
2020-03-10 20:44:40 +0100

    decode simple APRS messages possible

---
+++ src/APRSMessage.cpp
@@ -0,0 +1,115 @@
+#include "APRSMessage.h"
+
+APRSMessage::APRSMessage()
+	: _body(new APRSBody())
+{
+}
+
+APRSMessage::~APRSMessage()
+{
+	delete _body;
+}
+
+String APRSMessage::getSource() const
+{
+	return _source;
+}
+
+void APRSMessage::setSource(const String & source)
+{
+	_source = source;
+}
+
+String APRSMessage::getDestination() const
+{
+	return _destination;
+}
+
+void APRSMessage::setDestination(const String & destination)
+{
+	_destination = destination;
+}
+
+String APRSMessage::getPath() const
+{
+	return _path;
+}
+
+void APRSMessage::setPath(const String & path)
+{
+	_path = path;
+}
+
+APRSBody * const APRSMessage::getAPRSBody()
+{
+	return _body;
+}
+
+bool APRSMessage::decode(const String & message)
+{
+	int pos_src = message.indexOf('>');
+	_source = message.substring(0, pos_src);
+	int pos_dest = message.indexOf(',');
+	_destination = message.substring(pos_src+1, pos_dest);
+	int pos_path = message.indexOf(':');
+	_path = message.substring(pos_dest+1, pos_path);
+	_body->decode(message.substring(pos_path+1));
+	return true;
+}
+
+String APRSMessage::toString() const
+{
+	return "Source: " + _source + ", Destination: " + _destination + ", Path: " + _path + ", " + _body->toString();
+}
+
+/*
+APRSMessageTypes APRSMessage::getAPRSMessageType(const String & message)
+{
+	int blub = message.indexOf(':');
+	switch(message.charAt(blub + 1))
+	{
+		case '=':
+		case '!':
+			return APRSMessageTypes::PositionWithoutTimestamp;
+		case '@':
+		case '/':
+			return APRSMessageTypes::PositionWithTimestamp;
+		case '>':
+			return APRSMessageTypes::Status;
+		case '?':
+			return APRSMessageTypes::Query;
+		case ':':
+			return APRSMessageTypes::Message;
+		case '_':
+			return APRSMessageTypes::Weather;
+		case 'T':
+			return APRSMessageTypes::Telemetry;
+	}
+	return APRSMessageTypes::Message;
+}
+*/
+
+
+APRSBody::APRSBody()
+{
+}
+
+APRSBody::~APRSBody()
+{
+}
+
+String APRSBody::getData() const
+{
+	return _data;
+}
+
+bool APRSBody::decode(const String & message)
+{
+	_data = message;
+	return true;
+}
+
+String APRSBody::toString() const
+{
+	return "Data: " + _data;
+}
---
+++ src/APRSMessage.h
@@ -0,0 +1,70 @@
+#ifndef APRS_HEADER_H_
+#define APRS_HEADER_H_
+
+#include <Arduino.h>
+
+class APRSBody
+{
+public:
+	APRSBody();
+	virtual ~APRSBody();
+
+	String getData() const;
+	void setData(const String & data);
+
+	virtual bool decode(const String & message);
+	virtual String toString() const;
+
+private:
+	String _data;
+};
+
+class APRSMessage
+{
+public:
+	APRSMessage();
+	virtual ~APRSMessage();
+
+	String getSource() const;
+	void setSource(const String & source);
+
+	String getDestination() const;
+	void setDestination(const String & destination);
+
+	String getPath() const;
+	void setPath(const String & path);
+
+	APRSBody * const getAPRSBody();
+
+	virtual bool decode(const String & message);
+	virtual String toString() const;
+
+	/*
+	enum class APRSMessageTypes
+	{
+		PositionWithoutTimestamp,	// = and !
+		PositionWithTimestamp,		// @ and /
+		Status,						// >
+		Query,						// ?
+		Message,					// :
+		Weather,					// _
+		Telemetry,					// T
+		// you can add more types ;)
+	};
+
+	enum class APRSPositionTypes
+	{
+		WithMessage,	// = and @
+		NoMessage,		// ! and /
+	};*/
+
+private:
+	String _source;
+	String _destination;
+	String _path;
+	APRSBody * const _body;
+	//APRSMessageTypes _type;
+};
+
+
+#endif
