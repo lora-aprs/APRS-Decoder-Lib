542836e7290681aa72a536457bf844db1fd6f3a2
Peter Buchegger <peter.buchegger7@gmail.com>
2021-12-13 21:53:14 +0000

    check for MessageType

--- test/test_factory/test_factory.cpp
+++ test/test_factory/test_factory.cpp
@@ -28,6 +28,7 @@ void test_PositionFactory_double2NMEA() {
 void test_FactoryNotKnown() {
   String                                 msg = "AB1CDE-10>APRS:AABCDE";
   std::shared_ptr<aprs::NotKnownMessage> pos = std::static_pointer_cast<aprs::NotKnownMessage>(aprs::Factory::generate(msg));
+  TEST_ASSERT_TRUE(pos->getType() == aprs::MessageType::Error);
   TEST_ASSERT_EQUAL_STRING("AB1CDE-10", pos->getSource().c_str());
   TEST_ASSERT_EQUAL_STRING("APRS", pos->getDestination().c_str());
   TEST_ASSERT_EQUAL_STRING("ABCDE", pos->getText().c_str());
@@ -36,6 +37,7 @@ void test_FactoryNotKnown() {
 void test_FactoryPositionIn1() {
   String                          msg = "AB1CDE-10>APRS:=1234.12N/12345.12E-QTH von AB1CDE";
   std::shared_ptr<aprs::Position> pos = std::static_pointer_cast<aprs::Position>(aprs::Factory::generate(msg));
+  TEST_ASSERT_TRUE(pos->getType() == aprs::MessageType::PositionWithoutTimestamp);
   TEST_ASSERT_EQUAL_STRING("AB1CDE-10", pos->getSource().c_str());
   TEST_ASSERT_EQUAL_STRING("APRS", pos->getDestination().c_str());
   TEST_ASSERT_EQUAL_DOUBLE(12.56866666666666666667, pos->getLatitude());
@@ -46,6 +48,7 @@ void test_FactoryPositionIn1() {
 void test_FactoryPositionIn2() {
   String                          msg = "AB1CDE-10>APRS,AB1CDE:=1234.12N/12345.12E-QTH von AB1CDE";
   std::shared_ptr<aprs::Position> pos = std::static_pointer_cast<aprs::Position>(aprs::Factory::generate(msg));
+  TEST_ASSERT_TRUE(pos->getType() == aprs::MessageType::PositionWithoutTimestamp);
   TEST_ASSERT_EQUAL_STRING("AB1CDE-10", pos->getSource().c_str());
   TEST_ASSERT_EQUAL_STRING("APRS", pos->getDestination().c_str());
   for (auto &path : pos->getPath().get()) {
